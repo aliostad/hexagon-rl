3166b582bbc5682cdbe58c794ecfbbc83b011ac0
AliKheyrollahi <aliostad+github@gmail.com>
2018-09-10 10:31:48 +0100

    against random50

--- dqn_centaur_ai_gym.py
+++ dqn_centaur_ai_gym.py
@@ -252,7 +252,7 @@ class HierarchicalCentaurEnv(Env):
       self.shortMemory.append(World([]))
 
     self.centaur = SuperCentaurPlayer(EnvDef.centaur_name)
-    self.players = [self.centaur, Aliostad('random70', 70)]
+    self.players = [self.centaur, Aliostad('random50', 50)]
     shuffle(self.players)
     self.game = Game(EnvDef.game_name, self.players, radius=5)
     hexagon_ui_api.games[EnvDef.game_name] = self.game
