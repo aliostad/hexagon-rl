bf1005e0b085d69c490b8c901e4ff17c4e2ec1a9
AliKheyrollahi <aliostad+github@gmail.com>
2018-10-14 08:19:56 +0100

    even smaller

--- dqn_centaur_ai_gym.py
+++ dqn_centaur_ai_gym.py
@@ -25,7 +25,7 @@ class EnvDef:
   SHORT_MEMORY_SIZE = 1
   MAX_ROUND = 2000
   CELL_FEATURE = 1
-  MAX_GRID_LENGTH = 8
+  MAX_GRID_LENGTH = 5
   SPATIAL_INPUT = (MAX_GRID_LENGTH, MAX_GRID_LENGTH)
   SPATIAL_OUTPUT = (MAX_GRID_LENGTH * MAX_GRID_LENGTH, )
   EPISODE_REWARD = 1000
@@ -33,7 +33,7 @@ class EnvDef:
   DONT_OWN_MOVE_REWARD = -5
   CANT_ATTACK_MOVE_REWARD = -3
   GAME_VERBOSE=False
-  RADIUS = 4
+  RADIUS = 3
 # __________________________________________________________________________________________________________________________
 
 class NoneZeroEpsGreedyQPolicy(EpsGreedyQPolicy):
