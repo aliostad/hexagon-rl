d1a680eab05b7e8bd4424f089e32076ed80c0824
AliKheyrollahi <aliostad+github@gmail.com>
2018-11-09 15:12:39 +0000

    getting centaur gym running

--- centaur_gym.py
+++ centaur_gym.py
@@ -1,30 +1,33 @@
-from centaur import *
 from hexagon_agent import *
+import hexagon_ui_api
+import time
 
 class CentaurGym:
   def __init__(self):
     self.players = [
-      Aliostad('random05', 0.5),
-      Aliostad('random03', 0.3),
-      Aliostad('Ali-1'),
-      Centaur('centaur', 'zibolon')
+      Aliostad('Interpid Ibex', 0.5),
+      Aliostad('Jaunty Jackalope', 0.3),
+      Aliostad('Karmic Koala'),
+      Aliostad('Natty Narwhal', 0.9)
     ]
     self.game = None
 
-  def start(self, rounds=1000):
-    self.game = Game('my-game', self.players, 8)
-    self.game.start()
-    for i in range(0, rounds):
-      stats, finished = self.game.run_sync()
-      for s in stats:
-        print(s)
-      if finished:
-        break
+  def start(self, games=10, rounds=1000):
+    for gid in range(0, games):
+      self.game = Game('my-game', self.players, 6)
+      hexagon_ui_api.games['1'] = gym.game
+      self.game.start()
+      for i in range(0, rounds):
+        stats, finished, info = self.game.run_sync()
+        time.sleep(0.05)
+        if finished:
+          break
 
   def finish(self):
     self.game.finish()
 
 if __name__ == '__main__':
   gym = CentaurGym()
+  hexagon_ui_api.run_in_background()
   gym.start()
   gym.finish()
