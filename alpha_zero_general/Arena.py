f989aaf299c96f8fa25ff5dcd9cf2ed400a0969b
AliKheyrollahi <aliostad+github@gmail.com>
2018-11-16 13:34:01 +0000

    finishing off

--- alpha_zero_general/Arena.py
+++ alpha_zero_general/Arena.py
@@ -43,12 +43,12 @@ class Arena():
                 assert(self.display)
                 #print("Turn ", str(it), "Player ", str(curPlayer))
                 self.display(board)
-            action = players[curPlayer+1](self.game.getCanonicalForm(board, curPlayer))
-
-            valids = self.game.getValidMoves(self.game.getCanonicalForm(board, curPlayer), 1, curPlayer)
+            canon = self.game.getCanonicalForm(board, curPlayer)
+            action = players[curPlayer+1](canon)
+            valids = self.game.getValidMoves(canon, 1, curPlayer)
 
             if valids[action]==0:
-                print('hoy! wrong action {} {}', action, valids)
+                print('hoy! wrong action: {}'.format(action))
                 action = len(valids) - 1
             board, curPlayer = self.game.getNextState(board, curPlayer, action, executing=True)
         if verbose:
