204ab60a4f441f4a49890ac973523642fcc208ac
Ali Kheyrollahi <aliostad@gmail.com>
2019-10-09 15:31:09 +0100

    idiota

--- game_runner.py
+++ game_runner.py
@@ -3,23 +3,18 @@ import time
 
 
 class GameRunner:
-  def __init__(self, players, slotName):
-    '''
-
-    :type players: list of Player
-    '''
-    self.players = players
+  def __init__(self, slot):
+    self.slot = slot
     self.game = None
-    self.slot = Slot(slotName, players)
 
   def run_many(self, games=10, rounds=1000, radius=None):
     for gid in range(0, games):
       name = str(gid+1)
-      self.game = Game(self.slot.name, name, self.players, rounds, radius)
+      self.game = Game(self.slot.name, name, self.slot.players, rounds, radius)
       self.game.start()
       for i in range(0, rounds):
         stats, finished, info = self.game.run_sync()
-        time.sleep(0.05)
+        time.sleep(0.01)
         if finished:
           gs = GameStat(name, i, stats, finished)
           self.slot.add_game_stats(gs)
